cfa82387e39d02590ebbb5abb917de55bec82a59
Outdoor-Dev <saltairpix@gmail.com>
2021-01-09 15:04:18 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { Miami } from "./Miami";
-import { Cover } from "./Cover";
-import { Wynwood } from "./Wynwood";
-import { Watersports } from "./Watersports";
-import { NightLife } from "./NightLife";
-import { Everglades } from "./Everglades";
-import { Navbar } from "./Navbar";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
