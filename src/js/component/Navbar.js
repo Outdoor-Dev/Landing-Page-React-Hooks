d2ae23e485e45f34db6bb726883bb8e31e2c87e7
Outdoor-Dev <saltairpix@gmail.com>
2021-01-09 14:56:21 +0000

    update

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -2,99 +2,51 @@ import React from "react";
 
 export const Navbar = () => {
 	return (
-		<>
-			<nav className="navbar navbar-expand-lg navbar-light bg-black ">
-				<div className="container-fluid ">
-					<a className="navbar-brand" href="#">
-						Navbar
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon" />
-					</button>
-					<div
-						className="collapse navbar-collapse"
-						id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<a
-									className="nav-link active text-light"
-									aria-current="page"
-									href="#">
-									Home
-								</a>
-							</li>
-							<li className="nav-item text-light">
-								<a className="nav-link" href="#">
-									Link
-								</a>
-							</li>
-							<li className="nav-item dropdown">
-								<a
-									className="nav-link dropdown-toggle text-light"
-									href="#"
-									id="navbarDropdown"
-									role="button"
-									data-bs-toggle="dropdown"
-									aria-expanded="false">
-									Dropdown
-								</a>
-								<ul
-									className="dropdown-menu"
-									aria-labelledby="navbarDropdown">
-									<li>
-										<a
-											className="dropdown-item text-light"
-											href="#">
-											Action
-										</a>
-									</li>
-									<li>
-										<a
-											className="dropdown-item text-light"
-											href="#">
-											Another action
-										</a>
-									</li>
-									<li>
-										<hr className="dropdown-divider" />
-									</li>
-									<li>
-										<a
-											className="dropdown-item text-light"
-											href="#">
-											Something else here
-										</a>
-									</li>
-								</ul>
-							</li>
-							<li className="nav-item">
-								<a
-									className="nav-link disabled text-light"
-									href="#"
-									tabIndex="-1"
-									aria-disabled="true">
-									Disabled
-								</a>
-							</li>
-						</ul>
-						<form className="d-flex">
-							<input
-								className="form-control me-2"
-								type="search"
-								placeholder="Search"
-								aria-label="Search"
-							/>
-						</form>
-					</div>
+		<nav className="navbar navbar-expand-lg navbar-light bg-black">
+			<div className="container-fluid">
+				<span className="navbar-brand mb-0 h1 text-white">
+					Visit Miami
+				</span>
+				<div
+					className="collapse navbar-collapse"
+					id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item dropdown">
+							<a
+								className="nav-link dropdown-toggle"
+								href="#"
+								id="navbarDropdown"
+								role="button"
+								data-bs-toggle="dropdown"
+								aria-expanded="false">
+								<i className="fas fa-bars fa-lg" />
+							</a>
+							<ul
+								className="dropdown-menu"
+								aria-labelledby="navbarDropdown">
+								<li>
+									<a className="dropdown-item" href="#">
+										Action
+									</a>
+								</li>
+								<li>
+									<a className="dropdown-item" href="#">
+										Another action
+									</a>
+								</li>
+								<li>
+									<hr className="dropdown-divider" />
+								</li>
+								<li>
+									<a className="dropdown-item" href="#">
+										Something else here
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
 				</div>
-			</nav>
-		</>
+			</div>
+		</nav>
 	);
 };
